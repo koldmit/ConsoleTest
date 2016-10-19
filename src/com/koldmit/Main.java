35fa6cb2cbad09c953cae393de6a054a37f05ea4
koldmit <kol.dmit@gmail.com>
2016-10-19 19:16:24 +0300

    formatting

--- src/com/koldmit/Main.java
+++ src/com/koldmit/Main.java
@@ -3,7 +3,7 @@ package com.koldmit;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Console template");
+        System.out.println("Console template");
         System.out.println("Modify as you want");
     }
 }
